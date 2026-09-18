c1c9de5bd6f3edbf972ab86e0be3e4e1986541c7
Anes D <anofofa2@gmail.com>
2026-09-18 12:50:15 +0100

    drizzle prerender issue resolved

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import {relations} from './relations';
+import pg from "pg"
 
-export const db = drizzle(process.env.DATABASE_URL!, {relations});
+export const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL!,
+  max: 10, // Adjust based on your server limits
+});
+
+
+export const db = drizzle({client: pool, relations});

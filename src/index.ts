cc638bd2d1112b8d6c44ba0601b3225a32097c44
Anes D <anofofa2@gmail.com>
2026-09-18 13:05:08 +0100

    drizzle prerender issue resolved

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import pg from "pg"
 
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL!,
-  idleTimeoutMillis: 10000,
+  idleTimeoutMillis: 5000,
   max: 10, // Adjust based on your server limits
 });
 
